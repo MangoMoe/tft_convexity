c86b026dcd13f3279c335b767e6c6217938a157f
Dallin Clayton <dallin.clayton@gmail.com>
2020-03-04 21:22:21 -0700

    Cleaned up and made some progress

--- champion_scraper.py
+++ champion_scraper.py
@@ -1,49 +1,40 @@
 # %%
-import requests
+import httplib2
 from bs4 import BeautifulSoup
 
-#%%
-page = requests.get("https://app.mobalytics.gg/tft/set3/champions")
-soup = BeautifulSoup(page.content, 'html.parser')
 
 # %%
-# print(soup.prettify())
-# print(soup.prettify())
-print(len(soup.find_all('a')))
-
-
-# %%
-import httplib2
-from bs4 import BeautifulSoup, SoupStrainer
-
 http = httplib2.Http()
 status, response = http.request("https://app.mobalytics.gg/tft/set3/champions")
 
-# count = 0
-# for link in BeautifulSoup(response, parse_only=SoupStrainer('a')):
-#     count += 1
-#     # print(link)
-#     if link.__class__.__name__ == "Doctype":
-#         print("what the heck")
-#         continue
-#     if "href" in link.attrs:
-#         print(link['href'])
-# print(count)
-
 soup = BeautifulSoup(response)
+
+# %%
+def extract_champion_data(uri):
+    # TODO make the pandas dataframe (outside of here)
+    base_url = "https://app.mobalytics.gg"
+    url = base_url + uri
+    http = httplib2.Http()
+    status, response = http.request(url)
+    soup = BeautifulSoup(response)
+
+    h1 = soup.find('h1', {"class": "tft-champion-cardstyles__Name-sc-1ne6d17-10"})
+    name = h1.text
+    print(name)
+    # typography__Text16x700-sc-1ossu1y-9 tft-champion-cardstyles__Name-sc-1ne6d17-10 hpWIZv
+
+    div = soup.find('div', {"class": "markupstyles__SpreadBetweenLayout-bs8lb6-0"})
+    damages = div.find_all("p")[1].text.split(" / ")
+    print(damages)
+
+    div = soup.find('div', {"class": "champion-statsstyles__Wrapper-sc-1uyt2ip-0"})
+    healths = div.find("p").text.split(" / ")
+    print(healths)
+    # champion-statsstyles__Wrapper-sc-1uyt2ip-0 kkswzM
+
 
 for link in soup.find_all('a', href=True):
     if "set3/champions/" in link['href']:
-        print(link['href'])
-
-# %%
-from bs4 import BeautifulSoup
-import urllib.request
-
-# resp = urllib.request.urlopen("https://app.mobalytics.gg/tft/set3/champions")
-resp = urllib.request.urlopen("https://app.mobalytics.gg/tft/set3/champions/ahri")
-# status, resp = http.request('http://www.nytimes.com')
-soup = BeautifulSoup(resp, from_encoding=resp.info().get_param('charset'))
-
-for link in soup.find_all('a', href=True):
-    print(link['href'])
+        extract_champion_data(link['href'])
+        break
+        # print(link['href'])
